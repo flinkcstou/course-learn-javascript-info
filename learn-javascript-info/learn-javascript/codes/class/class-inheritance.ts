055eaea12e0234d964186508b52b780b49bebdf7
nabu <nabu@greet-go.com>
2023-04-03 09:19:04 +0600

    learn-javascript-info

--- learn-javascript-info/learn-javascript/codes/class/class-inheritance.ts
+++ learn-javascript-info/learn-javascript/codes/class/class-inheritance.ts
@@ -60,10 +60,28 @@ export class ClassInheritance {
     let rabbit = new Rabbit('Rabbit');
 
     console.error(rabbit);
-    console.error(Object.getOwnPropertyNames(Rabbit));
+    console.error(Object.getOwnPropertyNames(Rabbit));// constructor/ name/ length
+    console.error(Object.getOwnPropertyNames(Rabbit.prototype));// constructor/ name/ length
     // @ts-ignore
     console.error(Object.getOwnPropertyNames(Rabbit.prototype.__proto__));
+    console.error(Object.getOwnPropertyNames(Animal.prototype));
 
+    /*
+    Object.prototype = {toString, __proto__:null}
+    Function.prototype  = {... , __proto__: Object.prototype}
+
+    1) Empty class __proto__ = Function.prototype
+    2) Empty.prototype = ConstructorPrototype
+    3) Instance Empty class __proto = ConstructorPrototype
+    4) Animal class __proto__ = Function.prototype
+    5) Animal.prototype = {...ConstructorPrototype, run, stop, __proto__:Object.prototype}
+    6) Instance Animal class __proto__ = Animal.prototype
+    7) Rabbit class __proto__ = Function.prototype
+    8) Rabbit.prototype = {...ConstructorPrototype, hide, __proto__:Animal.prototype}
+    9) Instance Rabbit class __proto__ = Rabbit.prototype
+
+
+    * */
 
   }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClassInheritance } from '../../learn-javascript-info/learn-javascript/codes/class/class-inheritance';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn-javascript-info';
+
+  constructor() {
+
+    new ClassInheritance();
+  }
 }
