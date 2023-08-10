f198d7211cd9bff8713abe20384bcae8473bacf8
nabu <nabu@greet-go.com>
2023-08-10 18:12:45 +0600

    learn-javascript-info

--- learn-javascript-info/learn-javascript/codes/14-js-misc/_1-proxy.ts
+++ learn-javascript-info/learn-javascript/codes/14-js-misc/_1-proxy.ts
@@ -3,7 +3,118 @@ export class _1Proxy {
     // this.method1();
     // this.method2();
     // this.method3();
-    this.method4();
+    // this.method4();
+    // this.method5();
+    // this.method6();
+    // this.method7();
+    this.method8();
+  }
+
+  method8() {
+    function makeObservable(target) {
+
+      target['handlers'] = [];
+
+      target.observe = function(callB: (key, value) => void) {
+        target.handlers.push(callB);
+      };
+
+      return new Proxy(target, {
+        set(target: any, p: string | symbol, newValue: any, receiver: any): boolean {
+
+          target['handlers'].forEach((callback) => callback(p, newValue));
+          return Reflect.set(target, p, newValue, receiver);
+        }
+      });
+      /* ваш код */
+    }
+
+    let user: any = {};
+
+    user = makeObservable(user);
+
+    user.observe((key, value) => {
+      console.error(`SET ${key}=${value}`);
+    });
+    user.observe((key, value) => {
+      console.error(`SET ${key}=${value}`);
+    });
+
+    user.name = 'John'; // выводит: SET name=John
+  }
+
+  method7() {
+    function makeObservable(target) {
+
+      let callback = undefined;
+      target.observe = function(callB: (key, value) => void) {
+        callback = callB;
+      };
+
+      return new Proxy(target, {
+        set(target: any, p: string | symbol, newValue: any, receiver: any): boolean {
+          if (callback) {
+            callback(p, newValue);
+          }
+          return Reflect.set(target, p, newValue, receiver);
+        }
+      });
+      /* ваш код */
+    }
+
+    let user: any = {};
+
+    user = makeObservable(user);
+
+    user.observe((key, value) => {
+      console.error(`SET ${key}=${value}`);
+    });
+
+    user.name = 'John'; // выводит: SET name=John
+
+  }
+
+  method6() {
+    let array = [1, 2, 3];
+
+    array = new Proxy(array, {
+      get(target: number[], p: string, receiver: any): any {
+        if (+p >= 0) {
+          return Reflect.get(target, p, receiver);
+        } else {
+          return Reflect.get(target, target.length + (+p), receiver);
+
+        }
+      }
+    });
+
+    console.error(array[-1]);
+  }
+
+  method5() {
+    let user: any = {
+      name: 'John'
+    };
+
+    function wrap(target) {
+      return new Proxy(target, {
+
+        get(target: any, p: string | symbol, receiver: any): any {
+          if (target[p]) {
+            // @ts-ignore
+            let value = Reflect.get(...arguments);
+            return typeof value == 'function' ? value.bind(target) : value;
+          }
+          throw new Error('sdf');
+        }
+        /* ваш код */
+      });
+    }
+
+    user = wrap(user);
+
+    alert(user.name); // John
+    alert(user.age); // Ошибка: такого свойства не существует
   }
 
   method4() {
@@ -13,11 +124,11 @@ export class _1Proxy {
     str = new Proxy(str, {
       get(target: String, p: string | symbol, receiver: any): any {
         console.error(target, p, receiver);
-        return target[p].bind(target)
+        return target[p].bind(target);
       }
     });
 
-    console.error(str.toString())
+    console.error(str.toString());
     // console.error(str[0]);
     console.error(str);
   }
